73df33b4bfd9ab1614bab2f7c9c39a18ee4a22e8
Robolop42 <56067263+Robolop42@users.noreply.github.com>
2020-10-12 18:29:19 +0200

    Nice looking chat
    all in the name

--- html/chat.php
+++ html/chat.php
@@ -1,27 +1,52 @@
 <div id="chat_container">
-    <?php
-    $sql = "SELECT * FROM (SELECT * FROM `messages` ORDER BY messageid DESC LIMIT 5) Var1 ORDER BY messageid ASC";
-    $reponse = $base->query($sql);
+    <div id="chats">
+        <div id="loadmore">Load more</div>
+        <?php
+        $sql = "SELECT * FROM (SELECT * FROM `messages` ORDER BY messageid DESC LIMIT 50) Var1 ORDER BY messageid ASC";
+        $reponse = $base->query($sql);
 
-    while ($donnees = $reponse->fetch()) {
-        echo ("
-        <table>
-            <tr>
-                <td>" . $donnees['author'] . " </td>
-                <td>" . date("H:i", strtotime($donnees['date'])) . "</td>
-            </tr>
-            <tr>
-                <td>" . htmlspecialchars($donnees['message'], ENT_QUOTES, 'UTF-8') . "</td>
-            </tr>
-        </table>
-        <hr/>
-        ");
-    }
+        while ($donnees = $reponse->fetch()) {
+            echo ("
+        <div class='chatInfo ");
+        if ($donnees['author'] == $_SESSION['name'])
+         {
+            echo('myinfo');
+         };
+        echo("'>
+            <span class='msgdate'>" . date("H:i", strtotime($donnees['date'])) . "</span>");
+            if ($donnees['author'] != $_SESSION['name'])
+            {
+               echo("<span class='msgauthor'>" . $donnees['author'] . "</span>");
+            };
+            echo("
+        </div>
+        <div class='message ");
+        if ($donnees['author'] == $_SESSION['name'])
+         {
+            echo('mymessages');
+         };
+        echo("'>
+        " . htmlspecialchars($donnees['message'], ENT_QUOTES, 'UTF-8') . "
+        </div>");
 
-    $reponse->closeCursor();
+            // <table>
+            //     <tr>
+            //         <td>" .  . " </td>
+            //         <td>" . date("H:i", strtotime($donnees['date'])) . "</td>
+            //     </tr>
+            //     <tr>
+            //         <td>" . htmlspecialchars($donnees['message'], ENT_QUOTES, 'UTF-8') . "</td>
+            //     </tr>
+            // </table>
+            // <hr/>
+            // ");
+        }
 
-    ?>
+        $reponse->closeCursor();
 
+        ?>
+
+    </div>
     <div id='form' class='send_msg'>
         <form action='' method='post'>
             <input type='text' name='message'><br>
@@ -37,6 +62,13 @@
     echo $css;
     ?>
 </style>
+
+<script type="text/javascript">
+    window.onload = function() {
+        var objDiv = document.getElementById("chats");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
+</script>
 
 <!-- <div id='form' class=''>
         <form action='' method='post'>
